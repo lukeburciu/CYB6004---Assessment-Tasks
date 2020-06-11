fb0489164b61f7b72370e439d35bb08e7c611814
31502276 <luke.burciu@gmail.com>
2020-06-11 18:21:47 +0800

    minor changes

--- scripts/Week 4/IpAddressesOnly.sh
+++ scripts/Week 4/IpAddressesOnly.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#stores the IpInfo.sh file as a variable
 IpInfo="$(./IpInfo.sh)"
+
+#this command uses sed to accomplish the following:
+    # -n is used for silent modifications
+    #  s within the '/IP addr....' is used to substitute the line for a newer line
+    #  p within the sed command 'prints' the command
+    #  repeating 'IP Address:' twice is done to 1. Establish what line is being used/subd from IpInfo, and;
+        # 2. Substituting that with a new 'header'.
 
 ipaddress=$(echo "$IpInfo" | sed -n '/IP Address: / {s/IP Address:/IP Address:/p}')
 
+# this echos the information edited in ipaddress
 
 echo -e "IPs are: \n$ipaddress"
