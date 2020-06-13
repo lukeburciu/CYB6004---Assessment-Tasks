dd6b36ff6b70b06f003fa6e14aa8050d446774ee
31502276 <luke.burciu@gmail.com>
2020-06-13 22:01:58 +0800

    6.6 exercise completed

--- scripts/Week 6/BruteForceAttack.py
+++ scripts/Week 6/BruteForceAttack.py
@@ -1,42 +1,19 @@
 #!/usr/bin/python3
-import hashlib
-from itertools import chain, product
 
-def bruteForce(charset, maxLength):
-    return ( ''.join(candidate)
-        for candidate in chain.from_iterable(product(charset, repeat=i)
-        for i in range(1, maxLength + 1)))
+import string #imports string library from python
+import itertools #import itertools
+import hashlib #imports hashlib for use in the script
+ 
+# Brute-Force Password Cracker (2020). main.py. CyanCoding. https://github.com/CyanCoding/Brute-Force-Password-Cracker/tree/master/Python
 
-for attempt in bruteForce(string.ascii_lowercase, 5):
-    if matched:
-        break
-
-
-letterList = 'abcdefghijklmnopqrstuvwxyz'
-fullList = []
-
-for current in xrange(10):
-    a = [i for i in letterList]
-    for y in xrange(current):
-        a = [x+i for i in letterList for x in a]
-    fullList = letterList+a
-
-h = hashlib.sha256(b'')
-
-while 
-
-print(f"Trying the following hash: {word}: ")
-print(h.hexdigest() )
-
-#open the wordlist
-with open("wordlist.txt", "r") as wordlist:
-    #repeat for each word
-    for word in wordlist.readlines():
-        word = word.strip()
-        #hash the word
-        wordlistHash = hashlib.sha256(word.encode("utf-8")).hexdigest()
-        print(f"Trying password {word}:{wordlistHash}")
-        #if the hash is the same as the correct password's hash then we have cracked the password!
-        if(wordlistHash == passwordHash):
-            print(f"Password has been cracked! It was {word}")
+passwordHash = "8b7df143d91c716ecfa5fc1730022f6b421b05cedee8fd52b1fc65a96030ad52" # Supplied password hash to fit script
+alphanum = string.ascii_lowercase + string.digits # specifies [a-z][0-9] as the character sets
+for i in range(1, 5):   #loop through within range of 4 letters.
+    for letter in itertools.product(alphanum, repeat=i): # for the letter defined within range, repeat as many times as fits len
+        letter = ''.join(letter)                        #  appends new letter if a -> 9  is looped (e.g: AA,... AAA.. AAB....AA9 e.g e.g)
+        letterHash = hashlib.sha256(letter.encode("utf-8")).hexdigest() # Hashes above output into sha256sum
+        if(letterHash == passwordHash):     #if function to see if letterhash = password hash
+            print(f"Password has been cracked! It was '{letter}' !!")   #will print the following if true, break point
             break
+        else:                                                           # will print the following if false, subsequently looping back.
+            print(f"Trying password {letter}: {letterHash}")            
