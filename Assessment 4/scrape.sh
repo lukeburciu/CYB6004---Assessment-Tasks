8a9bd977a4c28a75dc681f54e381233155563815
31502276 <luke.burciu@gmail.com>
2020-06-14 11:45:07 +0800

    define env variables - minor change

--- Assessment 4/scrape.sh
+++ Assessment 4/scrape.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+#. ENV VARIABLES (API keys stored in .envrc.
+
+apiKey=$HYBRID_API_KEY
+apiSecret=$HYBRID_API_SECRET
+apiServer=$HYBRID_SERVER
+apiLogin=$HYBRID_LOGIN
+
+
+curl -v -H "apikey: $apiKey" $apiServer
