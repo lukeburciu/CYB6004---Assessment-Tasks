4497cce6d08742ef7b72dafa890294b95c7ad760
31502276 <luke.burciu@gmail.com>
2020-06-19 20:16:57 +0800

    minor

--- Assessment 4/ipreport.sh
+++ Assessment 4/ipreport.sh
@@ -13,28 +13,24 @@ echo -e "\e[93mSubmit IPv4 Address below..:\e[0m\n"
 echo -e "\e[35;1mIPs e.g: 0.0.0.0 - 255.255.255.255'\e[0m\n "
 
 #prompts User for IP address input.
+
 ipValid() { 
 
-   read -p "IP Address: " ip
+  # Set up local variables
+    local ip=${1:-1.2.3.4}
+    local IFS=.; local -a a=($ip)
 
-  #Regular expressions for IP address
-  #Checks to see if IP is valid
-    #if not a valid IP, will prompt to enter a valid IP referencing the function.
-    if expr "$ip" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' > /dev/null; then
-      IFS=.
-      set $ip
-      for quad in 1 2 3 4; do
-        if eval [ \$$quad -gt 255 ]; then
-          echo "Invalid IP address: ($ip). Please enter a valid IP address"
-          ipValid
-        fi
-      done
-      ipCheck
-    else
-        echo "Invalid IP address: ($ip). Please enter a valid IP address"
-        ipValid
-    fi
+    # Start with a regex format test
+    [[ $ip =~ ^[0-9]+(\.[0-9]+){3}$ ]] || return 1
+
+    # Test values of quads
+    local quad
+    for quad in {0..3}; do
+        [[ "${a[$quad]}" -gt 255 ]] && return 1
+    done
+    return 0
 }
+
 
   # ipCheck does the following:
   #  uses cURL to download report of the $url via a GET request of the VirusTotal API using an API key stored in .envrc. 
@@ -46,32 +42,36 @@ ipValid() {
 
 ipCheck()
 {
-  
+  read -p "IP Address: " ip
+  if ipValid "$ip"; then
 
-    echo -e "\n\e[34;1mValid IP: $ip... Looking up '$ip' against VirusTotal database\e[0m\n"
+      echo -e "\n\e[34;1mValid IP: $ip... Looking up '$ip' against VirusTotal database\e[0m\n"
 
-    apiDomain='https://www.virustotal.com/vtapi/v2/ip-address/report?'
-    apiKey="apikey=${VIRUSTOTAL_API_KEY}" #read env variable for API key
-    apiIP="&ip=${ip}"                     #specifies IP address
-    url="$apiDomain$apiKey$apiIP"         #concatenates outputs
+      apiDomain='https://www.virustotal.com/vtapi/v2/ip-address/report?'
+      apiKey="apikey=${VIRUSTOTAL_API_KEY}" #read env variable for API key
+      apiIP="&ip=${ip}"                     #specifies IP address
+      url="$apiDomain$apiKey$apiIP"         #concatenates outputs
       
-    mkdir -p ./response
-    mkdir -p ./report
-    curl --silent --request GET --url ${url} | jq -r '.' > ./response/ipresponse.json
-    cat ./response/ipresponse.json | jq --raw-output '.undetected_referrer_samples[] | [.date, .positives, .total, .sha256] | @csv' | tr -d '"' > ./report/ips/ipreport.csv
+      mkdir -p ./response
+      mkdir -p ./report
+      curl --silent --request GET --url ${url} | jq -r '.' > ./response/ipresponse.json
+      cat ./response/ipresponse.json | jq --raw-output '.undetected_referrer_samples[] | [.date, .positives, .total, .sha256] | @csv' | tr -d '"' > ./report/ips/ipreport.csv
       
-    #awk -F"," 'BEGIN { OFS = "," } {$5'
+      #awk -F"," 'BEGIN { OFS = "," } {$5'
       
-    printf "\n\n,,,,Report for: $ip. Undetected referrer samples - $now \n" >> ./report/ips/ipreporthistory.csv
-    cat ./report/ips/ipreport.csv >> ./report/ips/ipreporthistory.csv
+      printf "\n\n,,,,Report for: $ip. Undetected referrer samples - $now \n" >> ./report/ips/ipreporthistory.csv
+      cat ./report/ips/ipreport.csv >> ./report/ips/ipreporthistory.csv
       
-    echo -e "\e[46mUndetected referrer samples for $ip\e[0m\n"
-    ipreport="./report/ips/ipreport.csv" #specifies ipreport file for reportParser
-    sh ./config/reportParser.sh "$ipreport"
-    echo -e "\n\n\e[32;1mReport generated in: $ipreport\e[0m\n\n"
-    exit 0
-
+      echo -e "\e[46mUndetected referrer samples for $ip\e[0m\n"
+      ipreport="./report/ips/ipreport.csv" #specifies ipreport file for reportParser
+      sh ./config/reportParser.sh "$ipreport"
+      echo -e "\n\n\e[32;1mReport generated in: $ipreport\e[0m\n\n"
+      exit 0
+  else
+      echo "Invalid IP address: ($ip). Please enter a valid IP address"
+      ipCheck
+  fi
 }
 
 #start of the script
-ipValid
+ipCheck
